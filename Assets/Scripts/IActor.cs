64f355a78900c7db940dfd171ddc2c9392ad0b9f
AM <42077892+AndressMartin@users.noreply.github.com>
2021-03-05 15:50:00 -0300

    Revert "Implementação de novo input system"
    This reverts commit 8f2a171ebfc06284e9969f63b2c21da821b85e23.

--- Assets/Scripts/IActor.cs
+++ Assets/Scripts/IActor.cs
@@ -7,9 +7,6 @@ public interface IActor
     int PV { get; }
     int MN { get; }
     int PA { get; }
-    int PVMax { get; }
-    int MNMax { get; }
-    int PAMax { get; }
 
     int AlterarPV(int alteracao);
     int AlterarMN(int alteracao);
