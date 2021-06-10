1d3a0298f9c8b4a3d153d46d1c4381cae80800ab
TorroisBr <martinshenrique00@gmail.com>
2021-06-09 23:20:17 -0300

    imagem do Inventario/dialogo/status

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -7,14 +7,20 @@ public class DialogueTrigger : MonoBehaviour
 
     public Dialogue dialogue;
     public TextAsset textAsset;
+    public InputSys inputSys;
+
 
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+
+        inputSys.HideMenu(false);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
+        {
             TriggerDialogue();
+        }
     }
 }
